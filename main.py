2df3230997448e36f7d28b7c50b3651a0b82408f
Piasa <1005719151@qq.com>
2019-02-02 15:16:42 +0000

    change

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello")
+import json
+from pprint import pprint
+
+file = "/Users/gaoyifan/Downloads/CVDataset/cvDataset.json"
+fb = open(file, 'r')
+load_dict = json.load(fb)
+fb.close()
+pprint(load_dict)
